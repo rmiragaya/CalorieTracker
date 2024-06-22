b725dfa4474bca9cfaa4f657e8edc881d1f3f8ec
RodrigoMiragaya <rodrigo.miragaya@naranja.com>
2024-06-22 16:37:09 -0300

    funciona esta config para hilt, ok

--- buildSrc/src/main/java/DaggerHilt.kt
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -2,5 +2,4 @@ object DaggerHilt {
     const val version = "2.48.1"
     const val hiltAndroid = "com.google.dagger:hilt-android:$version"
     const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
-    const val hiltNoSeQue = "com.google.dagger:dagger-producers:$version"
 }
